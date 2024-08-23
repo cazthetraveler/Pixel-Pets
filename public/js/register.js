a819a3d0e570f04aba245dabb404060f310e8357
Luis Olmos <137588686+cazthetraveler@users.noreply.github.com>
2024-08-23 12:28:59 -0500

    added a check username availability

--- public/js/register.js
+++ public/js/register.js
@@ -4,7 +4,24 @@ const registerForm = async (event) => {
     const username = document.querySelector("#username-register").value.trim();
     const password = document.querySelector("#password-register").value.trim();
 
+    if (password.length < 8) {
+        alert('Password not strong enough!');
+        return;
+    };
+
     if (username && password) {
+        const checkUser = await fetch(`/api/users/check-availability?username=${encodeURIComponent(username)}`);
+        if (!checkUser.ok) {
+            alert('Enter a username!');
+            return;
+        }
+
+        const usernameData = await checkUser.json();
+        if (!usernameData.available) {
+            alert('Username already registered!');
+            return;
+        };
+
         const response = await fetch("/api/users", {
             method: "POST",
             body: JSON.stringify({username, password}),
