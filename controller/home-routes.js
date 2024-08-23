bdc2b39695a62448c0725876be3c49528da79b2e
Luis Olmos <137588686+cazthetraveler@users.noreply.github.com>
2024-08-23 16:57:20 -0500

    added user and explore pages.

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -48,6 +48,61 @@ router.get("/dashboard", async (req, res) => {
     };
 });
 
+router.get('/explore', async (req, res) => {
+    try {
+        if (!req.session.logged_in) {
+            res.redirect("/login");
+            return;
+        };
+
+        const userId = req.session.user_id;
+
+        const [usersData, userData] = await Promise.all([User.findAll(), User.findByPk(userId)]);
+
+        const users = usersData.map((user) => user.get({plain: true}))
+
+        res.render('explore', {logged_in: req.session.logged_in, users: users, username: userData.username});
+
+    } catch (error) {
+        res.status(500).json(error);
+    };
+});
+
+router.get("/user/:id", async (req, res) => {
+    try {
+        if (!req.session.logged_in) {
+            res.redirect("/login");
+            return;
+        };
+
+        const actualUserId = req.session.user_id;
+        const userId = req.params.id;
+
+        const [userData, actualUserData, petData] = await Promise.all([
+            User.findByPk(userId),
+            User.findByPk(actualUserId),
+            Pet.findAll({
+                where: {
+                    user_id: userId,
+                },
+            }),
+        ]);
+
+        if (!userData) {
+            res.status(404).json({message: 'The user does not exist!'});
+            return;
+        };
+
+        console.log(userData);
+
+        const pets = petData.map((pet) => pet.get({ plain: true }));
+
+        res.render("user", {logged_in: req.session.logged_in, username: actualUserData.username, user: userData.username, petCount:userData.petCount, pets: pets});
+    } catch (error) {
+        res.status(500).json(error);
+    };
+});
+
 router.get("/store", async (req, res) => {
     try {
         if (!req.session.logged_in) {
