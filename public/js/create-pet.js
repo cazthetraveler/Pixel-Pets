7314989e4db829e08b9581fdcc820950fd884434
MultiMouse <olmos.luisd@outlook.com>
2024-08-23 18:25:48 -0500

    fixed modal close buttons

--- public/js/create-pet.js
+++ public/js/create-pet.js
@@ -1,67 +1,79 @@
 const addPetBtn = document.querySelector("#add-pet");
 const petCreation = document.querySelector(".pet-creation");
-const closeBtnCreate = document.querySelector("#create-close");
+const closeBtnCreate = document.querySelector(".close-modal-button");
 
 const foods = ["Cheese", "Carrot", "Soop", "Cookie", "Lettuce"];
 
-addPetBtn.addEventListener("click", function() {
-    petCreation.style.display = "block";
+addPetBtn.addEventListener("click", function () {
+  petCreation.style.display = "block";
 });
 
-closeBtnCreate.addEventListener("click", function() {
-    petCreation.style.display = "none";
+closeBtnCreate.addEventListener("click", function () {
+  petCreation.style.display = "none";
 });
 
 function faveFood(foods) {
-    return Math.floor(Math.random() * foods.length);
-};
+  return Math.floor(Math.random() * foods.length);
+}
 
 function filterFood(foods, excluded) {
-    const filterArray = foods.filter(food => food !== excluded);
-    const randomFood = faveFood(filterArray);
-    return filterArray[randomFood];
+  const filterArray = foods.filter((food) => food !== excluded);
+  const randomFood = faveFood(filterArray);
+  return filterArray[randomFood];
 }
 
 const petForm = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const pet_type = document.querySelector("[name='pet-type']:checked").value;
-    const pet_name = document.querySelector("#pet-name").value.trim();
-    const pet_color = document.querySelector("#pet-color").value;
+  const pet_type = document.querySelector("[name='pet-type']:checked").value;
+  const pet_name = document.querySelector("#pet-name").value.trim();
+  const pet_color = document.querySelector("#pet-color").value;
 
-    const favorite_food = foods[faveFood(foods)];
-    const hated_food = filterFood(foods, favorite_food);
+  const favorite_food = foods[faveFood(foods)];
+  const hated_food = filterFood(foods, favorite_food);
 
-    console.log(`A ${pet_type} named ${pet_name}.`);
+  console.log(`A ${pet_type} named ${pet_name}.`);
 
-    if (pet_type && pet_name) {
-        const response = await fetch("/api/pets", {
-            method: "POST",
-            body: JSON.stringify({pet_type, pet_color, pet_name, favorite_food, hated_food}),
-            headers: {"Content-Type": "application/json"},
-        });
+  if (pet_type && pet_name) {
+    const response = await fetch("/api/pets", {
+      method: "POST",
+      body: JSON.stringify({
+        pet_type,
+        pet_color,
+        pet_name,
+        favorite_food,
+        hated_food,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if (response.ok) {
-            document.location.replace("/dashboard");
-        } else {
-            const errorData = await response.json();
-            alert(`Failed to create pet! ${JSON.stringify(errorData)}`);
-        };
-    };    
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      const errorData = await response.json();
+      alert(`Failed to create pet! ${JSON.stringify(errorData)}`);
+    }
+  }
 };
 
 document.querySelector("#create-pet-form").addEventListener("submit", petForm);
 
 function changeColor() {
-    let selectedColor = document.querySelector("#pet-color").value;
+  let selectedColor = document.querySelector("#pet-color").value;
 
-    let mousePortrait = document.querySelector("#mouse-portrait");
-    // let pigPortrait = document.querySelector("#pig-portrait");
-    // let tigerPortrait = document.querySelector("#tiger-portrait");
-    let penguinPortrait = document.querySelector("#penguin-portrait");
+  let mousePortrait = document.querySelector("#mouse-portrait");
+  // let pigPortrait = document.querySelector("#pig-portrait");
+  // let tigerPortrait = document.querySelector("#tiger-portrait");
+  let penguinPortrait = document.querySelector("#penguin-portrait");
 
-    mousePortrait.setAttribute("src", "/images/pets/mouse/" + selectedColor + "/pixel_mouse.png");
-    // pigPortrait.setAttribute("src", "/images/pets/pig/" + selectedColor + "/pixel_pig.png");
-    // tigerPortrait.setAttribute("src", "/images/pets/tiger/" + selectedColor + "/pixel_tiger.png");
-    penguinPortrait.setAttribute("src", "/images/pets/penguin/" + selectedColor + "/pixel_penguin.png");
-};
+  mousePortrait.setAttribute(
+    "src",
+    "/images/pets/mouse/" + selectedColor + "/pixel_mouse.png"
+  );
+  // pigPortrait.setAttribute("src", "/images/pets/pig/" + selectedColor + "/pixel_pig.png");
+  // tigerPortrait.setAttribute("src", "/images/pets/tiger/" + selectedColor + "/pixel_tiger.png");
+  penguinPortrait.setAttribute(
+    "src",
+    "/images/pets/penguin/" + selectedColor + "/pixel_penguin.png"
+  );
+}
--- public/js/delete-pet.js
+++ public/js/delete-pet.js
@@ -1,35 +1,37 @@
 const delPetBtn = document.querySelector("#delete-pet");
 const petDeletion = document.querySelector(".pet-deletion");
-const closeBtnDelete = document.querySelector("#delete-close");
+const closeBtnDelete = document.querySelector(".close-modal-button");
 
-delPetBtn.addEventListener("click", function() {
-    petDeletion.style.display = "block";
+delPetBtn.addEventListener("click", function () {
+  petDeletion.style.display = "block";
 });
 
-closeBtnDelete.addEventListener("click", function() {
-    petDeletion.style.display = "none";
+closeBtnDelete.addEventListener("click", function () {
+  petDeletion.style.display = "none";
 });
 
 const delPetForm = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const id = document.querySelector("#sel-pet-del").value;
-    console.log(id);
+  const id = document.querySelector("#sel-pet-del").value;
+  console.log(id);
 
-    if (id) {
-        const response = await fetch("/api/pets", {
-            method: "DELETE",
-            body: JSON.stringify({id}),
-            headers: {"Content-Type": "application/json"},
-        });
+  if (id) {
+    const response = await fetch("/api/pets", {
+      method: "DELETE",
+      body: JSON.stringify({ id }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if (response.ok) {
-            document.location.replace("/dashboard");
-        } else {
-            const errorData = await response.json();
-            alert(`Failed to delete pet! ${JSON.stringify(errorData)}`);
-        };
-    };    
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      const errorData = await response.json();
+      alert(`Failed to delete pet! ${JSON.stringify(errorData)}`);
+    }
+  }
 };
 
-document.querySelector("#delete-pet-form").addEventListener("submit", delPetForm);
+document
+  .querySelector("#delete-pet-form")
+  .addEventListener("submit", delPetForm);
